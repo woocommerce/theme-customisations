d19b5a431b6d78c796709a619a79c27d8e9fee0c
James Koster <james@jameskoster.co.uk>
2015-03-05 17:19:00 +0000

    order..

--- custom/functions.php
+++ custom/functions.php
@@ -1,6 +1,7 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
 /**
  * Functions.php
  * Add php snippets here
  */
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
