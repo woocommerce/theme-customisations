e0b2e95bce1c179cac304f0a4e57e6e12aedb348
James Koster <james@jameskoster.co.uk>
2015-06-18 10:52:52 +0100

    empty js. closes #2

--- theme-customisations.php
+++ theme-customisations.php
@@ -30,6 +30,7 @@ final class Theme_Customisations {
 	 */
 	public function theme_customisations_setup() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'theme_customisations_css' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'theme_customisations_js' ) );
 
 		require_once( 'custom/functions.php' );
 	}
@@ -39,7 +40,15 @@ final class Theme_Customisations {
 	 * @return void
 	 */
 	public function theme_customisations_css() {
-		wp_enqueue_style( 'customisations', plugins_url( '/custom/style.css', __FILE__ ) );
+		wp_enqueue_style( 'custom-css', plugins_url( '/custom/style.css', __FILE__ ) );
+	}
+
+	/**
+	 * Enqueue the Javascript
+	 * @return void
+	 */
+	public function theme_customisations_js() {
+		wp_enqueue_script( 'custom-js', plugins_url( '/custom/custom.js', __FILE__ ), array( 'jquery' ) );
 	}
 
 } // End Class
